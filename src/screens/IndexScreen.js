e8475dabf93ad56c472698d744bed0351ef19fb4
Kit <kit@Kits-MacBook-Pro.local>
2022-03-25 17:33:21 -0700

    Blog post add post working delete post not hitting switch

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,24 +1,47 @@
 import React, {useContext} from 'react'
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native'
-import BlogContext from '../context/BlogContext'
-
+import {Context} from '../context/BlogContext'
+import {Feather} from '@expo/vector-icons'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const IndexScreen = () => {
-    const {data, addBlogPost} = useContext(BlogContext)
+    const {state, addBlogPost, deleteBlogPost} = useContext(Context)
 
     return  <View>
-            <Text> Index IndexScreen</Text>
             <Button title="Add Post" onPress={addBlogPost}/>
             <FlatList 
-            data= {data}
+            data= {state}
             keyExtractor={(blogPost) => blogPost.title}
             renderItem={({item}) => {
-                return <Text> {item.title}</Text>
+                return <View style= {styles.row}>
+                    <Text style={styles.title}> 
+                    {item.title} - {item.id}
+                    </Text>
+                    <TouchableOpacity onPress={() => deleteBlogPost}>
+                    <Feather name="trash" style={styles.icon}/>
+                    </TouchableOpacity>
+                    </View>
             }}
             />
         </View>
     
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+})
 export default IndexScreen
